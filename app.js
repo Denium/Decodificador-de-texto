9940a0c46d9ee556488a8b778e6ffa95528ddf8f
valdenio cosme <valdenio.junior@hotmail.com>
2024-03-26 00:55:46 -0300

    Criptografador

--- app.js
+++ app.js
@@ -1,70 +1,90 @@
-const textArea = document.querySelector("#textarea__apresentacao");
-const inputResultDecrypt = document.querySelector("#textarea__resultado");
-const btn_criptografar = document.querySelector(".criptografarBtn");
-const btn_descriptografar = document.querySelector(".descriptografarBtn");
-const btn_copy = document.querySelector("#btn_copy");
+function mostrarBotaoCopiar() {
+  let botao = document.querySelector('.result_btn');
+  botao.style.display = 'block';
+}
 
-function onChangeTextAreaResultado() {
-  let textAreaResultado = document.getElementById("textarea__resultado");
-  let imgLupa = document.getElementById("imgLupa");
-  let msgNenhuma = document.getElementById("msgNenhuma");
+function ocultarSecaoInformativa() {
+  let boneco = document.querySelector('.homemLupa');
+  let nenhuma = document.querySelector('.nenhuma__mensagem');
+  boneco.style.display = 'none';
+  nenhuma.style.display = 'none';
+}
 
-  if (
-    textAreaResultado.value &&
-    textAreaResultado.value !== textAreaResultado.defaultValue
-  ) {
-    imgLupa.style.display = "none";
-    msgNenhuma.style.display = "none";
+function exibirTexto (textoParametro) {
+  let secaoTexto = document.querySelector('#textarea__resultado')
+  secaoTexto.style.display = 'block'
+  secaoTexto.innerHTML = textoParametro;
+  return textoParametro;
+}
+
+function copiarTexto() {
+  let textoParaCopiar = document.querySelector('#textarea__resultado').innerHTML;;
+  let botao = document.querySelector('.result_btn');
+  navigator.clipboard.writeText(textoParaCopiar);
+  botao.innerHTML = 'Copiado <i class="fa-solid fa-check"></i>';
+}
+
+const statusBotaoCopiar = () => {
+  let botao = document.querySelector('.result_btn');
+  botao.innerHTML = '<i class="fa-solid fa-copy"></i> Copiar'
+}
+
+const controleDeExibicao = () => {
+  ocultarSecaoInformativa();
+  mostrarBotaoCopiar();
+}
+
+function encriptar() {
+  let input = document.querySelector('#textarea__apresentacao');
+  let texto = input.value;
+  if (texto.trim() === '') {
+      input.setAttribute('placeholder', 'Você precisa digitar algo aqui!')
   } else {
-    imgLupa.style.display = "block";
-    msgNenhuma.style.display = "block";
+      let codificado = texto.replace(/e/g, 'enter').replace(/i/g, 'imes').replace(/a/g, 'ai').replace(/o/g, 'ober').replace(/u/g, 'ufat');
+      exibirTexto(codificado);
+      controleDeExibicao();
+      statusBotaoCopiar();
+      let maiusculas = checarMaiusculas(texto);
+      let especiais = checarCaracteresEspeciais(texto);
+      sinalizandoMaiuscOuCaractEspec(maiusculas || especiais);
+      input.value = ''
+      return true;
   }
 }
 
-function criptografar(string) {
-  const result = string
-    .replace(/e/gi, "enter")
-    .replace(/i/gi, "imes")
-    .replace(/a/gi, "ai")
-    .replace(/o/gi, "ober")
-    .replace(/u/gi, "ufat");
-
-  return result;
-}
-
-function descriptografar(string) {
-  const result = string
-    .replace(/enter/gi, "e")
-    .replace(/imes/gi, "i")
-    .replace(/ai/gi, "a")
-    .replace(/ober/gi, "o")
-    .replace(/ufat/gi, "u");
-
-  return result;
-}
-
-btn_criptografar.addEventListener("click", () => {
-  const result = criptografar(textArea.value);
-  inputResultDecrypt.value = result;
-  onChangeTextAreaResultado();
-});
-
-btn_descriptografar.addEventListener("click", () => {
-  const result = descriptografar(textArea.value);
-  inputResultDecrypt.value = result;
-  onChangeTextAreaResultado();
-});
-
-btn_copy.addEventListener("click", () => {
-  try {
-    navigator.clipboard.writeText(inputResultDecrypt.value);
-    console.log("texto copiado!");
-  } catch {
-    console.log("falha");
+function descriptar() {
+  let input = document.querySelector('#textarea__apresentacao');
+  let texto = input.value;
+  if (texto.trim() === ''){
+      input.setAttribute('placeholder', 'Você precisa digitar algo aqui!')
+  } else {
+      let decodificado = texto.replace(/enter/g, 'e').replace(/imes/g, 'i').replace(/ai/g, 'a').replace(/ober/g, 'o').replace(/ufat/g, 'u');
+      exibirTexto(decodificado);
+      controleDeExibicao();
+      statusBotaoCopiar();
+      let maiusculas = checarMaiusculas(texto);
+      let especiais = checarCaracteresEspeciais(texto);
+      sinalizandoMaiuscOuCaractEspec(maiusculas || especiais);
+      input.value = ''
   }
-});
+}
 
+function checarMaiusculas(texto) {
+  for (let i = 0; i < texto.length; i++) {
+      if (texto[i] !== texto[i].toLowerCase()) {
+          sinalizandoMaiuscOuCaractEspec()
+          return true
+      }
+  }
+}
 
+function checarCaracteresEspeciais(texto) {
+  const regex = /[^a-zA-Z0-9\s]/g;
+  if (regex.test(texto)) {
+      sinalizandoMaiuscOuCaractEspec()
+      return true
+  }
+}
 
 
 // Crytografia verdadeira exemplo:
